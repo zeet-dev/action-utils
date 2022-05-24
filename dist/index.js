e5b9d9c1f435c359a0216ce87533117dbd80b9a3
hi019 <65871571+hi019@users.noreply.github.com>
2022-05-24 19:13:08 -0400

    Updates

---
+++ dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.installBinary = void 0;
+var cli_1 = require("./cli");
+Object.defineProperty(exports, "installBinary", { enumerable: true, get: function () { return cli_1.installBinary; } });
